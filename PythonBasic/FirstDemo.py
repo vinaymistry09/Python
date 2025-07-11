6af448793c1b79ed0973f6f27a0df365ad17686b
vinaymistry09 <61341223+vinaymistry09@users.noreply.github.com>
2025-07-10 22:30:33 -0300

    First Python Project

---
+++ PythonBasic/FirstDemo.py
@@ -0,0 +1,24 @@
+print("hello")
+
+#here are the comments i have defined
+
+a = 3
+print (a)
+
+Str = "Hello world"
+print(Str)
+
+b,c,d = 5,6.5,"Great"
+
+# print("Value is",b)
+print("{} {}".format("Value is",b))
+
+print(type(b))
+print(type(c))
+print(type(d))
+
+
+
+
+
+
